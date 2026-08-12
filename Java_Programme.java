3ed6093888da1204b76208a2461b4a1c0fbb2bee
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-12 22:08:36 +0530

    Update Java_Programme.java

--- Java_Programme.java
+++ Java_Programme.java
@@ -1,1 +1,12 @@
+//String Reverse Programme
 
+String str="Ishwar";
+String rev="";
+int len=str.length();
+
+for(int i=len-1; i>=0; i--)
+  {
+     rev=rev+str.charAt(i);
+  }
+
+System.out.println(rev);

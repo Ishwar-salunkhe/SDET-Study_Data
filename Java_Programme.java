418603bcefcf886112da588abad6845b12e6a1d4
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-13 17:07:10 +0530

    Implement integer reversal and palindrome check
    Added a program to reverse an integer and check for palindrome.

--- Java_Programme.java
+++ Java_Programme.java
@@ -10,3 +10,29 @@ for(int i=len-1; i>=0; i--)
   }
 
 System.out.println(rev);
+
+===============x==============x================x==========================x=
+
+  //Int Number Reverse Progarmme And Check Number is Palindrome or Not
+
+        int num=12321;
+        int orgNum=num;
+        int rev=0;
+
+        while(num!=0)
+        {
+            rev=rev*10+num%10;
+            num=num/10;
+        }
+
+        //System.out.println(rev);
+        if(rev==orgNum)
+        {
+            System.out.println(orgNum+" Palindorme Number");
+        }
+        else
+        {
+            System.out.println(orgNum+" No Palindrome");
+        }
+
+======================================x=====================================================

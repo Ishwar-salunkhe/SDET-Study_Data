3a3d5122bf64e4b0b89c7048bf286f87c5590101
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-19 23:21:25 +0530

    Update Int_JavaProgramme.java

--- Int_JavaProgramme.java
+++ Int_JavaProgramme.java
@@ -1,1 +1,21 @@
+//Pyramid Number Patern Programme
 
+int num = 1;
+for (int i = 1; i <= 4; i++)
+  {
+      // Print spaces
+      for (int j = 1; j <= 4 - i; j++)
+      {
+          System.out.print(" ");
+       }
+
+      // Print numbers
+      for (int j = 1; j <= i; j++)
+        {
+           System.out.print(num + " ");
+            num++;
+        }
+
+     System.out.println();
+
+ }

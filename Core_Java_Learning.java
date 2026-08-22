222959a917eb9e8eb4126a2354d0a7750a3d9675
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-22 23:00:34 +0530

    Update and rename Core_Java_Learning.txt to Core_Java_Learning.java

---
+++ Core_Java_Learning.java
@@ -0,0 +1,10 @@
+//Collection
+-Collection used to store Group of elements
+-Collection is an root interface and this is extended in the another interfaces such as List and set interface
+-List interface implemented in the Array List class and Set implemented in the HashSet Class
+-We can not store primitive data type directly in the collection 
+-If we want to store then we can use wrapper class
+-It is coming from package- java.util
+
+//ArrayList
+ArrayList

4916b84faab1ed1c4744cb631b83970ee27ddb02
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-29 20:04:35 +0530

    Update Int_JavaProgramme.java

--- Int_JavaProgramme.java
+++ Int_JavaProgramme.java
@@ -81,3 +81,28 @@ System.out.println("After swapping b: "+b);
 System.out.println("After swapping "+a);
 
 =========================================x==========================================================
+      
+  //Count even and odd numner
+      int num=12345;
+      int even=0;
+      int odd=0;
+
+      while(num>0)
+      {
+        int digit=num%10;
+        if(digit%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        
+        num=num/10;
+      }
+
+      System.out.println("even number count is "+ even);
+      System.out.println("odd number count is "+ odd);
+
+===============================x==================================================================

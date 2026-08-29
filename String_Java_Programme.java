c1d97b142d4c4c0baaa80729c8be611dc726a05a
Ishwar Salunkhe <salunkheishwar1510@gmail.com>
2026-08-29 15:54:05 +0530

    Update String_Java_Programme.java

--- String_Java_Programme.java
+++ String_Java_Programme.java
@@ -13,28 +13,7 @@ System.out.println(rev);
 
 ===============x==============x================x==========================x=
 
-  //Int Number Reverse Progarmme And Check Number is Palindrome or Not
-
-int num=12321;
-int orgNum=num;
-int rev=0;
-
-   while(num!=0)
-      {
-          rev=rev*10+num%10;
-          num=num/10;
-      }
-
-      //System.out.println(rev);
-      if(rev==orgNum)
-      {
-          System.out.println(orgNum+" Palindorme Number");
-      }
-      else
-      {
-          System.out.println(orgNum+" No Palindrome");
-      }
-
+ 
 ======================================x=====================================================
 
       //Find Duplicate Charcters in String
